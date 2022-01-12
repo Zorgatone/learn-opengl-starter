f3b495c8998c2db7350b8f9f5aca046187c6584a
Zorgatone <zorgatone@gmail.com>
2022-01-12 21:48:01 +0100

    Added OpenGL hello work example: triangles

---
+++ include/vgl.h
@@ -0,0 +1,15 @@
+//
+// Created by zorgatone on 12/01/2022.
+//
+
+#ifndef TEST_GLFW_VGL_H
+#define TEST_GLFW_VGL_H
+
+#include <GL/gl3w.h>
+#include <GL/gl.h>
+
+#include <GLFW/glfw3.h>
+
+#define BUFFER_OFFSET(a) ((void*)(a))
+
+#endif //TEST_GLFW_VGL_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,71 @@
 #include <iostream>
+#include <filesystem>
 
-#include <GL/gl3w.h>
-#include <GLFW/glfw3.h>
+#include "vgl.h"
+#include "LoadShaders.h"
+
+
+enum VAO_IDs {
+    Triangles,
+    NumVAOs // This must be the last value of the enum
+};
+
+enum Buffer_IDs {
+    ArrayBuffer,
+    NumBuffers // This must be the last value of the enum
+};
+
+enum Attrib_IDs {
+    vPosition = 0
+};
+
+// Eww global variables
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
+
+const GLuint NumVertices = 6;
+
+void init() {
+    static const GLfloat vertices[NumVertices][2] = {
+            {-0.90, -0.90},
+            {0.85,  -0.90},
+            {-0.90, 0.85},
+            {0.90,  -0.85},
+            {0.90,  0.90},
+            {-0.85, 0.90}
+    };
+
+    glCreateBuffers(NumBuffers, Buffers);
+    glNamedBufferStorage(Buffers[ArrayBuffer], sizeof(vertices), vertices, 0);
+
+    ShaderInfo shaders[] = {
+            {GL_VERTEX_SHADER,   "shaders/triangles.vert"},
+            {GL_FRAGMENT_SHADER, "shaders/triangles.frag"},
+            {GL_NONE,            nullptr}
+    };
+
+    GLuint program = LoadShaders(shaders);
+    glUseProgram(program);
+
+    glGenVertexArrays(NumVAOs, VAOs);
+    glBindVertexArray(VAOs[Triangles]);
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+    glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+    glEnableVertexAttribArray(vPosition);
+}
+
+void display() {
+    static const float black[] = {0.0f, 0.0f, 0.0f, 0.0f};
+    glClearBufferfv(GL_COLOR, 0, black);
+
+    glBindVertexArray(VAOs[Triangles]);
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+}
 
 int main(int argc, char *argv[]) {
     using namespace std;
 
-    GLFWwindow *window;
+    cout << "Current path: " << filesystem::current_path() << endl;
 
     /* Initialize the library */
     if (!glfwInit()) {
@@ -14,7 +73,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Triangles", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -24,22 +83,26 @@ int main(int argc, char *argv[]) {
     glfwMakeContextCurrent(window);
 
     if (gl3wInit()) {
-        cerr <<  "Failed to initialize OpenGL" << endl;
+        cerr << "Failed to initialize OpenGL" << endl;
         return -1;
     }
 
     if (!gl3wIsSupported(3, 2)) {
-        cerr <<  "OpenGL 3.2 not supported" << endl;
+        cerr << "OpenGL 3.2 not supported" << endl;
 
         return -1;
     }
 
     cout << "OpenGL" << glGetString(GL_VERSION) << ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION);
 
+    cout.flush();
+
+    init();
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        /* Render */
+        display();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -48,6 +111,8 @@ int main(int argc, char *argv[]) {
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
